69fb35f7af62e6aea1d43ab5cc4de0ad92237035
Fadhlan Fariz Makarim <fadhlanfariz95@gmail.com>
2021-09-13 20:45:43 +0700

    template soal day 18-19

---
+++ 18/index.js
@@ -0,0 +1,22 @@
+// const express = require('express')
+// const app = express()
+// const port = 3000
+// app.get('/', function(req, res) {
+//   res.send("students") {
+//     "students"= [
+//       {         "name": "Jihad",         "gender": "Male"       },
+//       {         "name": "Joshua",         "gender": "Male"       },
+//       {         "name": "Rayan",         "gender": "Male"       },
+//       {         "name": "Rayhan",         "gender": "Male"       },
+//       {         "name": "Rizky",         "gender": "Male"       },
+//       {         "name": "Vasya",         "gender": "Female"       },
+//       {         "name": "Wahid",         "gender": "Male"       },
+//     ]
+//   }
+
+// app.listen(port, () => console.log('Example app listening at http://localhost/:$%7Bport%7D%27'))
+
+// 3 modul untuk 3 bulan (software clinic)
+// Javascript, update
+
+// 1 modul untuk dijual di udemy
